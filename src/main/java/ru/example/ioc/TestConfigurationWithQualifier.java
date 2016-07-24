e44e1357d260569f472a95eac6b7d42687269c87
Ilia Kuchmin <ikuchmin@gmail.com>
2016-07-24 15:41:51 +0400

    Qualifiers were added to arguments of methods

--- src/main/java/ru/example/ioc/TestConfigurationWithQualifier.java
+++ src/main/java/ru/example/ioc/TestConfigurationWithQualifier.java
@@ -1,13 +1,14 @@
 package ru.example.ioc;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by ikuchmin on 24.07.16.
  */
-//@Configuration
-public class TestConfiguration {
+@Configuration
+public class TestConfigurationWithQualifier {
     @Bean
     public Integer m1() {
         return 42;
@@ -19,22 +20,22 @@ public class TestConfiguration {
     }
 
     @Bean
-    public Integer m3(Integer m1) {
+    public Integer m3(@Qualifier("m1") Integer m1) {
         return 42;
     }
 
     @Bean
-    public Integer m4(Integer m3) {
+    public Integer m4(@Qualifier("m3") Integer m3) {
         return 42;
     }
 
     @Bean
-    public Integer m5(Integer m4, Integer m2) {
+    public Integer m5(@Qualifier("m4") Integer m4, @Qualifier("m2") Integer m2) {
         return 42;
     }
 
     @Bean
-    public Integer m6(Integer m3, Integer m2) {
+    public Integer m6(@Qualifier("m3") Integer m3, @Qualifier("m2") Integer m2) {
         return 42;
     }
 }
